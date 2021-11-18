8b860f551cf168659a1eb1dda5dc5b82af19f35e
Kevin Lockerby <klockerby@onestreamsoftware.com>
2021-11-18 17:53:31 -0500

    Add project files.

---
+++ MvvmKL/FormMain.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace MvvmKL
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbList = new System.Windows.Forms.ListBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(71, 64);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(71, 122);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(71, 216);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lbList
+            // 
+            this.lbList.FormattingEnabled = true;
+            this.lbList.Location = new System.Drawing.Point(285, 64);
+            this.lbList.Name = "lbList";
+            this.lbList.Size = new System.Drawing.Size(164, 186);
+            this.lbList.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(189, 13);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 374);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lbList);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ListBox lbList;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ MvvmKL/ViewModels/FormMainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace MvvmKL.ViewModels
+{
+    public class FormMainViewModel : INotifyPropertyChanged
+    {
+        private string logonResult;
+        public virtual string LogonResult
+        {
+            get { return logonResult; }
+            set
+            {
+                if (logonResult != value)
+                {
+                    logonResult = value;
+                    RaisePropertyChanged("LogonResult");
+                }
+            }
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
